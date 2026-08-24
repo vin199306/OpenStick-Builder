50c9ab2a2e5403b49747b488af79ff37469f56f4
vin199306 <vin199306@users.noreply.github.com>
2026-08-24 11:38:04 +0800

    fix: create dtbs/qcom dir before copying dtbs
    The linux-postmarketos-qcom-msm8916 package (which created
    /boot/dtbs/qcom) is no longer installed since we use prebuilt
    kernel modules, so mkdir the target before cp.

--- scripts/alpine_rootfs.sh
+++ scripts/alpine_rootfs.sh
@@ -174,6 +174,7 @@ mkdir -p ${CHROOT}/boot/extlinux
 cp configs/extlinux.conf ${CHROOT}/boot/extlinux
 
 # copy custom dtb's
+mkdir -p ${CHROOT}/boot/dtbs/qcom
 cp dtbs/* ${CHROOT}/boot/dtbs/qcom
 
 # copy gadget-tool templates and script
